8ba417f2731d67a6f0e0e3f39caf4eb53e1090d9
Muhammad540 <thisisdeahmed@gmail.com>
2024-07-19 16:49:37 +0300

    files added

---
+++ build/lifecycle_bringup/CMakeFiles/lifecycle_bringup_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for lifecycle_bringup_uninstall.
+# This may be replaced when dependencies are built.
---
+++ build/lifecycle_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/libgelistirici/practice/ros2_ws/install/lifecycle_py/share/lifecycle_py/package.sh"
